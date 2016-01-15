87d84ece65afb825904fa0a73f6b2d24d4980c93
Cory Green <1elitehacker@gmail.com>
2016-01-15 12:42:54 -0500

    RaycastTreeChoping
    TreeChoping, Attach to player.

---
+++ raycastChop.js
@@ -0,0 +1,25 @@
+#pragma strict
+
+private var hit : raycastHit;
+
+private var rayLength : int = 10;
+
+function Update () {
+  if(Input.GetMouseDown(1)) {
+    if(raycast.hit.on-collider("Tree")) {
+      Wait();
+      TreeController();
+    }
+  }
+}
+
+
+function Wait() {
+  yield WaitForSeconds (0.7);
+}
+
+function TreeController() {
+
+  var treeController : TreeContoller;
+    treeController.TreeChop;
+}
